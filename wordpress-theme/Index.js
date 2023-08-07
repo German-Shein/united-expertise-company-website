ec398468332da6bfa1b63261f9c96b30ba535027
German Shein <germanshein1995@gmail.com>
2023-08-07 17:42:13 +0400

    Creating the WordPress theme

---
+++ wordpress-theme/Index.js
@@ -0,0 +1,267 @@
+const International_Text = 
+{
+	Hero_Header_1: 
+	{
+		en: "Your Top Saudi Partner in Environmental Safety",
+		ar: "أفضل شريك سعودي لك في مجال السلامة البيئية"
+	},
+	Hero_Header_2: 
+	{
+		en: "Join us on a mission to build a cleaner world!",
+		ar: "!انضم إلينا في مهمة لبناء عالم أنظف"
+	},
+	Call_to_Action_1:
+	{
+		en: "Get a free quote",
+		ar: "الحصول على أسعار مجاني"
+	},
+	Call_to_Action_2:
+	{
+		en: "Send Message",
+		ar: "ارسل رسالة"
+	},
+	Feature_Header_1:
+	{
+		en: "Environmental Consultancy and Studies",
+		ar: "الاستشارات والدراسات البيئية"
+	},
+	Feature_Header_2:
+	{
+		en: "Agriculture and Natural Resource Services",
+		ar: "خدمات الزراعة والموارد الطبيعية"
+	},
+	Feature_Header_3:
+	{
+		en: "Environmental Services",
+		ar: "الخدمات البيئية"
+	},
+	Feature_Header_4:
+	{
+		en: "Training and Safety Services",
+		ar: "خدمات التدريب والسلامة"
+	},
+	Company_Description:
+	{
+		en: "United Expertise Company for Environmental Consultancy (UEC) provides environmental consulting and studies services to its valued clients to assess the environmental damages and provide the best economic solutions compatible with the principles of environmental safety by a highly qualified national (Saudi) team with more than 30 years of experience.",
+		ar: "تقدم شركة الخبرة المتحدة للاستشارات البيئية خدمات الاستشارات والدراسات البيئية لعملائها الكرام لتقييم الأضرار البيئية وتقديم أفضل الحلول الاقتصادية المتوافقة مع مبادئ السلامة البيئية من قبل فريق وطني مؤهل تأهيلا عاليا (سعودي) مع أكثر من ٣٠ عاما من الخبرة ."
+	},
+	Offer_Header:
+	{
+		en: "WE OFFER",
+		ar: "نحن نقدم"
+	},
+	Service_Card_Title_1:
+	{
+		en: "Agriculture and Natural Resource Services",
+		ar: "خدمات الزراعة والموارد الطبيعية"
+	},
+	Service_Card_Title_2:
+	{
+		en: "Environmental Services",
+		ar: "الخدمات البيئية"
+	},
+	Service_Card_Title_3:
+	{
+		en: "Training and Safety Services",
+		ar: "الخدمات البيئية"
+	},
+	Feature_1:
+	{
+		en: "Evaluating agricultural lands",
+		ar: "تقييم الأراضي الزراعية"
+	},
+	Feature_2:
+	{
+		en: "Soil and groundwater analysis",
+		ar: "تحليل التربة والمياه الجوفية"
+	},
+	Feature_3:
+	{
+		en: "Petrochemical Analysis",
+		ar: "تحليل البتروكيماويات"
+	},
+	Feature_4:
+	{
+		en: "Waste water management and analysis",
+		ar: "إدارة وتحليل مياه الصرف الصحي"	
+	},
+	Feature_5:
+	{
+		en: "Testing new products’ compliance with local and international standards",
+		ar: "اختبار امتثال المنتجات الجديدة للمعايير المحلية والدولية"
+	},
+	Feature_6:
+	{
+		en: "Field and laboratory environmental measurements",
+		ar: "القياسات البيئية الميدانية والمخبرية"
+	},
+	Feature_7:
+	{
+		en: "Operation, maintenance and supply of laboratories",
+		ar: "تشغيل وصيانة وتوريد المعامل"
+	},
+	Feature_8:
+	{
+		en: "Studies of the environmental impact of industrial, agricultural and food processes",
+		ar: "دراسات الأثر البيئي للعمليات الصناعية والزراعية والغذائية"
+	},
+	Feature_9:
+	{
+		en: "Environmental management programs",
+		ar: "برامج الإدارة البيئية"
+	},
+	Feature_10:
+	{
+		en: "Develop and manage certified industrial facilities",
+		ar: "تطوير وإدارة المنشآت الصناعية المعتمدة"
+	},
+	Feature_11:
+	{
+		en: "Environmental monitoring programs",
+		ar: "برامج المراقبة البيئية"
+	},
+	Feature_12:
+	{
+		en: "Monitoring stations’ supply and operation",
+		ar: "مراقبة توريد وتشغيل المحطات"
+	},
+	Feature_13:
+	{
+		en: "Medical, Industrial, Agricultural waste management programs",
+		ar: "برامج إدارة النفايات الطبية والصناعية والزراعية"
+	},
+	Feature_14:
+	{
+		en: "Field assessment of air, soil, water and marine environments",
+		ar: "التقييم الميداني للهواء والتربة والماء والبيئات البحرية"
+	},
+	Feature_15:
+	{
+		en: "Environmental awareness through campaigns and exhibitions",
+		ar: "التوعية البيئية من خلال الحملات والمعارض"
+	},
+	Feature_16:
+	{
+		en: "Programs to develop skills for the environmentally-friendly work environment",
+		ar: "برامج لتنمية المهارات لبيئة العمل الصديقة للبيئة"
+	},
+	Feature_17:
+	{
+		en: "Fieldwork teams for training and environmental awareness",
+		ar: "فرق العمل الميداني للتدريب والتوعية البيئية"
+	},
+	Feature_18:
+	{
+		en: "Environmental programs and courses for establishment training",
+		ar: "برامج ودورات بيئية للتدريب المؤسسي"
+	},
+	Feature_19:
+	{
+		en: "OSHA Standard Safety Training",
+		ar: "OSHA تدريب السلامة القياسية"
+	},
+	Feature_20:
+	{
+		en: "IS014001 Training",
+		ar: "IS014001 تدريب"
+	},
+	Feature_21:
+	{
+		en: "Specialized programs to raise awareness of consumer items impact",
+		ar: "برامج متخصصة للتوعية بأثر المواد الاستهلاكية"
+	},
+	Blog_Title_1:
+	{
+		en: "Our Story",
+		ar: "قصتنا"
+	},
+	Blog_Title_2:
+	{
+		en: "Company Update 1",
+		ar: "تحديث الشركة ١"
+	},
+	Blog_Title_3:
+	{
+		en: "Company Update 2",
+		ar: "تحديث الشركة ٢"
+	},
+	Blog_Date_1:
+	{
+		en: "July 2, 2023",
+		ar: "١٤ ذو الحجة ١٤٤٤"
+	},
+	Blog_Date_2:
+	{
+		en: "July 9, 2023",
+		ar: "٢١ ذو الحجة ١٤٤٤"
+	},
+	Blog_Date_3:
+	{
+		en: "July 16, 2023",
+		ar: "٢٨ ذو الحجة ١٤٤٤"
+	},
+	Contact_Us_Header:
+	{
+		en: "Start working with us now",
+		ar: "ابدأ العمل معنا الآن"
+	},
+	Label_1:
+	{
+		en: "Name",
+		ar: "اسم"
+	},
+	Label_2:
+	{
+		en: "Email",
+		ar: "رِسالة إيميل"
+	},
+	Label_3:
+	{
+		en: "Requested Service",
+		ar: "الخدمة المطلوبة"
+	},
+	Label_4:
+	{
+		en: "Message",
+		ar: "خطاب"
+	},
+	Copyright:
+	{
+		en: "© 2023 United Expertise Company. All rights reserved.",
+		ar: "جميع الحقوق محفوظة الخبرات المتحدة للاستشارات البيئية © 2023"
+	},
+	Terms_of_Use:
+	{
+		en: "Terms of Use",
+		ar: "تعليمات الاستخدام"
+	},
+	Privacy_Policy:
+	{
+		en: "Privacy_Policy",
+		ar: "سياسة الخصوصية"
+	}
+}
+
+const Toggle_Language = () =>
+{
+	const Headers = [...document.getElementsByTagName ('h1'), ...document.getElementsByTagName ('h2'), ... document.getElementsByTagName ('h3'), ...document.getElementsByTagName ('h4'), ...document.getElementsByTagName ('h5'), ...document.getElementsByTagName ('h6')]
+	const Language = document.documentElement.getAttribute ("lang");
+	const Text = [...document.getElementsByTagName ('p'), ...document.getElementsByTagName ('span'), ... document.getElementsByTagName ('div'), ...document.getElementsByTagName ('input'), ...document.getElementsByTagName ('textarea'), ...document.getElementsByTagName ('label'), ...document.getElementsByTagName ('li')]
+	document.documentElement.setAttribute ("lang", Language === "en" ? "ar" : "en");
+	Object.keys (International_Text).forEach (ID => document.getElementById (ID).innerHTML = International_Text [ID] [Language === "en" ? "ar" : "en"]);
+	if (Language === 'en')
+	{
+		document.getElementsByClassName ("Arabic_Language") [0].setAttribute ('style', '');
+		document.getElementsByClassName ("English_Language") [0].setAttribute ('style', 'display: none;');
+		Headers.forEach (Header => Header.classList.add ('Arabic_Header'))
+		Text.forEach (Text_Element => Text_Element.classList.add ('Arabic_Text'))
+
+	}
+	else if (Language === 'ar')
+	{
+		document.getElementsByClassName ("Arabic_Language") [0].setAttribute ('style', 'display: none;');
+		document.getElementsByClassName ("English_Language") [0].setAttribute ('style', '');
+		Headers.forEach (Header => Header.classList.remove ('Arabic_Header'))
+		Text.forEach (Text_Element => Text_Element.classList.remove ('Arabic_Text'))
+	}
+}
---
+++ wordpress-theme/footer.php
@@ -0,0 +1,9 @@
+        <footer class="Footer Padding_Left_4rem Padding_Right_4rem Padding_Top_1rem Padding_Bottom_1rem Background_Color_2 Text_Color_2">
+			<p class="Footer_Copyright" id="Copyright">© 2023 United Expertise Company. All rights reserved.</p>
+			<div class="Footer_Links">
+				<a id="Terms_of_Use" href="#">Terms of Use</a>
+				<a id="Privacy_Policy">Privacy Policy</a>
+			</div>
+		</footer>
+	</body>
+</html>
