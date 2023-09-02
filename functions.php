0574ff85a15ca05f1d965ef998406ae4ac410ba5
German Shein <germanshein1995@gmail.com>
2023-09-03 00:12:43 +0400

    Reducing image sized and adding minified JavaScript and CSS

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@ function Add_CSS ()
 {
 	wp_register_style ('Fonts', get_template_directory_uri () . '/styles/Fonts.css', false);
 	wp_enqueue_style ('Fonts');
-	wp_register_style ('Global', get_template_directory_uri () . '/styles/Global.css', false);
+	wp_register_style ('Global', get_template_directory_uri () . '/styles/Global.min.css', false);
 	wp_enqueue_style ('Global');
-	wp_register_style ('Index', get_template_directory_uri () . '/styles/Index.css', false);
+	wp_register_style ('Index', get_template_directory_uri () . '/styles/Index.min.css', false);
 	wp_enqueue_style ('Index');
-	wp_register_style ('Utility', get_template_directory_uri () . '/styles/Utility.css', false);
+	wp_register_style ('Utility', get_template_directory_uri () . '/styles/Utility.min.css', false);
 	wp_enqueue_style ('Utility');
 }
 
@@ -16,7 +16,7 @@ add_action ('wp_enqueue_scripts', 'Add_CSS');
 
 function Add_JavaScript ()
 {
-	wp_register_script ('Index', get_template_directory_uri () . '/Index.js', true);
+	wp_register_script ('Index', get_template_directory_uri () . '/Index.min.js', true);
 	wp_enqueue_script ('Index');
 }
 add_action ('wp_enqueue_scripts', 'Add_JavaScript');
